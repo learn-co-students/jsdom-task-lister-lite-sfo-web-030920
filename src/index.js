f9c4752200db87b17bf280a9fe601f437041c00c
caicindy87 <caicindy87@gmail.com>
2020-04-20 17:29:32 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+  form.addEventListener("submit", addItemToList);
+  // const allItems = document.getElementsByClassName('list-item'); 
 });
+
+// function deleteButton() {
+//   const deleteButton = document.createElement('button');
+//   deleteButton.textContent = 'x';
+//   item.append(deleteButton);
+// }
+
+function addItemToList(event) {
+  const ul = document.getElementById('tasks');
+  const item = document.createElement('li');
+  item.className = 'list-item'
+  event.preventDefault();
+  const listItem = ul.appendChild(item);
+  const input = document.getElementById('new-task-description').value;
+  listItem.textContent = input;
+  // deleteButton();
+}
+
+// function deleteItem(event) {
+  
+// }
